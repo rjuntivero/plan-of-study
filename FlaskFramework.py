48feebf65b9f2377f0ed7ac06c6ba33f209293a2
Brandon-Rocha <156843936+Brandon-Rocha@users.noreply.github.com>
2024-03-20 13:33:44 -0400

    Update FlaskFramework.py
    Added first version of search, should query based on Course.cname

--- FlaskFramework.py
+++ FlaskFramework.py
@@ -1,7 +1,7 @@
 from Database import Base, Course
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -16,6 +16,16 @@ def index():
     # Query courses from the database
     courses = session.query(Course).all()
     return render_template('index.html', courses=courses)
+
+@app.route('/search', methods = ['index.html'])
+def search():
+    #search courses by name 
+    search_term = request.form.get('search_term')
+    if search_term:
+        courses = session.query(Course).filter(Course.cname.like(f%'%{search_term}%')).all()
+        return render_template('results.html', courses = courses)
+    else:
+        return jsonify({'error': 'Search term is required'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
