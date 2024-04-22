bb616bcfca6150b6967d0fdf6fdb72155dd31fc8
RJ <161075475+rjuntivero@users.noreply.github.com>
2024-04-21 21:43:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, Blueprint, jsonify, Response, make_response, redirect
+from flask import Flask, render_template, request, send_file, Blueprint, jsonify, Response, make_response, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from Database import Base, Course
 from sqlalchemy import create_engine
@@ -7,6 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from flask_mail import Mail, Message
 from PyPDF2 import PdfReader, PdfWriter
 from PyPDF2.generic import NameObject, createStringObject
+import math
 import os, io
 
 app = Flask(__name__)
@@ -31,6 +32,28 @@ mail = Mail(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/reset-completed-attributes')
+def reset_completed_attributes_route():
+    reset_completed_attributes()  # Reset completed attributes
+    return 'Completed attributes reset successfully.'
+    
+def reset_completed_attributes():
+    # Open a new database session
+    db_sesh = DBSession()
+    try:
+        # Reset all completed attributes to False in the database
+        db_sesh.query(Course).update({Course.completed: False})
+        db_sesh.commit()
+        print("Completed attributes reset successfully.")
+    except Exception as e:
+        # Handle exceptions
+        db_sesh.rollback()
+        print(f"An error occurred: {str(e)}")
+    finally:
+        # Close the session
+        db_sesh.close()
 
 #Required Courses List
 @app.route('/get-required-courses')
@@ -59,57 +82,64 @@ def send_email():
     return "Message sent!"
 
 #Search Bar with Filters
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET','POST'])
 def search():
-    if request.method == 'POST':
-        # Get the search query from the form data
-        search_query = request.form['search_query']
-        session = DBSession()
-        
-        # Extract filter options from html (takes in 'name')
-        filter1 = request.form.get('filter1')
-        filter2 = request.form.get('filter2')
-        filter3 = request.form.get('filter3')
+    session = DBSession()
+    default_message = "No Results Matching Search Criteria"
 
-        courses = []
+    print("Request Form Data: ", request.form)
+    # Get the search query from the form data
+    search_query = request.form.get('search_query', '')
+    
+    # Extract filter options from html (takes in 'name')
+    filter1 = request.form.get('filter1')
+    filter2 = request.form.get('filter2')
+    filter3 = request.form.get('filter3')
 
-        print("Search Query:", search_query)
-        print("Filter 1:", filter1)
-        print("Filter 2:", filter2)
-        print("Filter 3:", filter3)
-        # Base query
-        query = session.query(Course)
-        
-        # Subject Filter
-        if filter1 and filter1 != "Subject":
-            print("Applying Filter 1:", filter1)
-            query = query.filter(Course.class_id.startswith(filter1))
-        if filter2 and filter2 not in ["", "Semester"]:
-            # Semester Filter
-            print("Applying Filter 2:", filter2)
-            if filter2 == "Fall":
-                query = query.filter(Course.fall == 1)
-            elif filter2 == "Spring":
-                query = query.filter(Course.spring == 1)
-            elif filter2 == "Summer":
-                query = query.filter(Course.summer == 1)
-        if search_query:
-            # Prioritize course with matching names
-            query = query.filter(Course.cname.ilike(f'%{search_query}%'))
-        
-        #debug statement
-        count_before_filter = query.count()
-        print("Count of courses before filter:", count_before_filter)
-        
-        courses = query.all()
-        
-        #debug statement
-        count_after_filter = len(courses)
-        print("Count of courses after filter:", count_after_filter)
-        
-        session.close()
-        
-        return render_template('search_results.html', courses=courses, search_query=search_query)
+    courses = []
+
+    print("Search Query:", search_query)
+    print("Filter 1:", filter1)
+    print("Filter 2:", filter2)
+    print("Filter 3:", filter3)
+
+    # Base query
+    query = session.query(Course)
+    
+    # Subject Filter
+    if filter1 and filter1 != "Subject":
+        print("Applying Filter 1:", filter1)
+        query = query.filter(Course.class_id.startswith(filter1))
+    if filter2 and filter2 not in ["", "Semester"]:
+        # Semester Filter
+        print("Applying Filter 2:", filter2)
+        if filter2 == "Fall":
+            query = query.filter(Course.fall == 1)
+        elif filter2 == "Spring":
+            query = query.filter(Course.spring == 1)
+        elif filter2 == "Summer":
+            query = query.filter(Course.summer == 1)
+    if filter3 and filter3.strip():
+        print("Applying Filter 3:", filter3)
+        query = query.filter(Course.class_id.contains(filter3))
+    if search_query:
+        # Prioritize course with matching names
+        query = query.filter(Course.cname.ilike(f'%{search_query}%'))
+    
+    #debug statement
+    #count_before_filter = query.count()
+    #print("Count of courses before filter:", count_before_filter)
+
+    courses = query.all()
+    #debug statement
+    #count_after_filter = len(courses)
+    #print("Count of courses after filter:", count_after_filter)
+    
+    session.close()
+    if not courses:
+        return render_template('search_results.html', default_message=default_message)
+
+    return render_template('search_results.html', courses=courses, search_query=search_query)
 
 #Add button for Course Catalog Results
 @app.route('/add-course', methods=['POST'])
@@ -124,7 +154,6 @@ def add_course():
             # Fetch the course to be updated
             course_to_update = db_sesh.query(Course).filter_by(class_id=course_id).first()
             
-            # Update the completed attribute to True
             course_to_update.completed = True
             
             # Commit the changes to the database
@@ -133,7 +162,7 @@ def add_course():
             # Close the session
             db_sesh.close()
             
-            # Redirect back to the same page
+            # Return a JSON response indicating success
             return redirect(request.referrer)
         
         except Exception as e:
@@ -141,25 +170,23 @@ def add_course():
             return redirect(request.referrer)
 
 #PDF Generation (POS Generation) NOT YET IMPLEMENTED/WORKING
+# Plan of Study PDF Generation
 @app.route('/generate_pdf', methods=['GET','POST'])
-def generate_pdf():
-    try:
-        # Retrieve form data
-        course_id = request.form.get('course_id', '')
-        course_name = request.form.get('course_name', '')
-        
-        #debug statement
-        print("Received form data:")
-        print("Course ID:", course_id)
-        print("Course Name:", course_name)
+def generatePlanOfStudy():
+    # Retrieve form data
+    name = request.form.get('NameField')  # Student name
+    student_number = request.form.get('N-Number')  # Student number
 
+    try:
         # Define path to PDF template
         template_path = os.path.join(app.root_path, 'static', 'POS_Template.pdf')
+
+        # Create a PdfWriter object to write to the output PDF
+        writer = PdfWriter()
 
         # Open the preexisting PDF template
         with open(template_path, 'rb') as template_file:
             reader = PdfReader(template_file)
-            writer = PdfWriter()
 
             # Fill in form fields with data
             for page_num in range(len(reader.pages)):
@@ -171,46 +198,27 @@ def generate_pdf():
                             field_name = annot['/T']
                             if isinstance(field_name, bytes):
                                 field_name = field_name.decode('utf-8')
-                            # Remove leading and trailing slash and parenthesis
                             field_name = field_name.strip('(/)').strip()
-                            print("Field name:", field_name)
-                            field_value = None
-                            if annot.get('/V') is not None:
-                                # Extract field value
-                                field_value_obj = annot['/V'].get_object()
-                                if '/AS' in field_value_obj:
-                                    field_value = field_value_obj['/AS']
-                                else:
-                                    field_value = field_value_obj
-                                print("Field value:", field_value)
-                            if field_value is not None:
-                                if 'Semester' in field_name and 'Row' in field_name:
-                                    # Fill in Semester fields
-                                    annot.update({
-                                        NameObject("/V"): createStringObject("random fill in the spots")
-                                    })
-                                    print("Semester field filled.")
-                                elif 'CreditsRow' in field_name:
-                                    # Fill in CreditsRow fields
-                                    annot.update({
-                                        NameObject("/V"): createStringObject("random fill in the spots")
-                                    })
-                                    print("CreditsRow field filled.")
+
+                            # Fill out the classification checkbox field based on the selected year
+                            if field_name == 'StudentName':
+                                annot.update({NameObject("/V"): createStringObject(name)})
+                            elif field_name == 'StudentNumber':
+                                annot.update({NameObject("/V"): createStringObject(student_number)})
                 writer.add_page(page)
 
             # Define path for output filled PDF
-            output_pdf = "filled_plan_of_study.pdf"  
+            output_pdf = "filled_plan_of_study.pdf"
 
             # Write filled PDF to disk
             with open(output_pdf, 'wb') as output_file:
                 writer.write(output_file)
-
+        
         # Return the filled PDF as a response
-        response = make_response(send_file(output_pdf, as_attachment=True))
-        return response
+        return send_file(output_pdf, as_attachment=True)
 
     except Exception as e:
-        #exception debugging
+        # Handle exceptions
         print("Error:", str(e))
         return jsonify(success=False, error=str(e))
 
